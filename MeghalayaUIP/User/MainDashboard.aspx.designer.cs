b1879dc9aac98ce9bee78791ba912f2888491a79
kalyanbabuteja <kalyanbabu.ykb@gmail.com>
2024-05-15 21:38:22 +0530

    Main Dashboard design Done

--- MeghalayaUIP/User/MainDashboard.aspx.designer.cs
+++ MeghalayaUIP/User/MainDashboard.aspx.designer.cs
@@ -22,14 +22,5 @@ namespace MeghalayaUIP.User
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl unitname;
-
-        /// <summary>
-        /// gvPreRegUserDashboard control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView gvPreRegUserDashboard;
     }
 }
