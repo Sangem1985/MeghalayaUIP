3018f06ebefc33ba22705c32fb4486d93b2e3f40
kalyanbabuteja <kalyanbabu.ykb@gmail.com>
2024-05-29 11:42:15 +0530

    design

--- MeghalayaUIP/User/user.Master.designer.cs
+++ MeghalayaUIP/User/user.Master.designer.cs
@@ -114,6 +114,15 @@ namespace MeghalayaUIP.User
         protected global::System.Web.UI.WebControls.LinkButton lnkGrvStatus;
 
         /// <summary>
+        /// LinkButton1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton LinkButton1;
+
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
