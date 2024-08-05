68ef601b2cdb6827574c1c1317a1083bb30dec49
Sowjanya Chinni <sowjanya128@gmail.com>
2024-08-05 15:21:06 +0530

    info wiz

--- MeghalayaUIP/InformationWizard.aspx.designer.cs
+++ MeghalayaUIP/InformationWizard.aspx.designer.cs
@@ -87,6 +87,15 @@ namespace MeghalayaUIP
         protected global::System.Web.UI.WebControls.Button btnSearch;
 
         /// <summary>
+        /// gvInfoWiz control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gvInfoWiz;
+
+        /// <summary>
         /// UpdateProgress control.
         /// </summary>
         /// <remarks>
