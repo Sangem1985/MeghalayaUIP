69d011573edc47da3215e1806ad5a0eddde8926f
YADAGIRI <akhilakki058@gmail.com>
2024-09-20 13:57:02 +0530

    REPORTS PAGES

---
+++ MeghalayaUIP/Dept/Reports/IRDistWiseReport.aspx.cs
@@ -0,0 +1,202 @@
+﻿using MeghalayaUIP.BAL.CommonBAL;
+using MeghalayaUIP.BAL.ReportBAL;
+using MeghalayaUIP.Common;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MeghalayaUIP.Dept.Reports
+{
+    public partial class IRDistWiseReport : System.Web.UI.Page
+    {
+        MasterBAL mstrBAL = new MasterBAL();
+        ReportBAL reportsBAL = new ReportBAL();
+
+        int TotalAppl, ImaPending, ImaQuery, CommPending, CommApproved, CommRejected, CommQuery;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                FillGridData();
+                BindDistricts();
+            }
+        }
+
+        protected void ddldistrict_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        protected void BindDistricts()
+        {
+            try
+            {
+
+                ddldistrict.Items.Clear();
+                List<MasterDistrcits> objDistrictModel = new List<MasterDistrcits>();
+                string strmode = string.Empty;
+                strmode = "";
+                objDistrictModel = mstrBAL.GetDistrcits();
+                if (objDistrictModel != null)
+                {
+                    ddldistrict.DataSource = objDistrictModel;
+                    ddldistrict.DataValueField = "DistrictId";
+                    ddldistrict.DataTextField = "DistrictName";
+                    ddldistrict.DataBind();
+
+                }
+                else
+                {
+                    ddldistrict.DataSource = null;
+                    ddldistrict.DataBind();
+
+
+                }
+                AddSelect(ddldistrict);
+
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public void AddSelect(DropDownList ddl)
+        {
+            try
+            {
+                System.Web.UI.WebControls.ListItem li = new System.Web.UI.WebControls.ListItem();
+                li.Text = "--Select--";
+                li.Value = "0";
+                ddl.Items.Insert(0, li);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public void FillGridData()
+        {
+            try
+            {
+                DataSet ds = new DataSet();
+                ds = reportsBAL.DistrictWiseReports(ddldistrict.SelectedValue, ddlEnterPriseType.SelectedItem.Text, txtFormDate.Text, txtToDate.Text);
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    GVDistrictWise.DataSource = ds.Tables[0];
+                    GVDistrictWise.DataBind();
+
+                }
+                else
+                {
+                    GVDistrictWise.DataSource = null;
+                    GVDistrictWise.DataBind();
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        protected void GVDistrictWise_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                int Application = Convert.ToInt32(DataBinder.Eval(e.Row.DataItem, "TOTALAPPL"));
+                TotalAppl = Application + TotalAppl;
+
+                int Pending = Convert.ToInt32(DataBinder.Eval(e.Row.DataItem, "IMAPENDING"));
+                ImaPending = Pending + ImaPending;
+
+                int Query = Convert.ToInt32(DataBinder.Eval(e.Row.DataItem, "IMAQUERYRAISED"));
+                ImaQuery = Query + ImaQuery;
+
+                int CommPendings = Convert.ToInt32(DataBinder.Eval(e.Row.DataItem, "COMMPENDING"));
+                CommPending = CommPendings + CommPending;
+
+                int Approved = Convert.ToInt32(DataBinder.Eval(e.Row.DataItem, "COMMAPPROVED"));
+                CommApproved = Approved + CommApproved;
+
+                int Rejected = Convert.ToInt32(DataBinder.Eval(e.Row.DataItem, "COMMREJECTED"));
+                CommRejected = Rejected + CommRejected;
+
+                int CommonQuery = Convert.ToInt32(DataBinder.Eval(e.Row.DataItem, "COMMQUERY"));
+                CommQuery = CommonQuery + CommQuery;
+
+
+
+                Label lblDist = (Label)e.Row.FindControl("lblDistrictid");
+                LinkButton lnkTotal = (LinkButton)e.Row.FindControl("lblTotal");
+                LinkButton lnkPending = (LinkButton)e.Row.FindControl("lblPending");
+                LinkButton lnkQuery = (LinkButton)e.Row.FindControl("lblQueryRaised");
+                LinkButton lnkcommpending = (LinkButton)e.Row.FindControl("lblCommPending");
+                LinkButton lnkApproved = (LinkButton)e.Row.FindControl("lblCommApproved");
+                LinkButton lnkRejected = (LinkButton)e.Row.FindControl("lblCommRejected");
+                LinkButton lnkCommQuery = (LinkButton)e.Row.FindControl("lblCommQuery");
+
+                //if (lnkTotal.Text != "0")
+                //    lnkTotal.PostBackUrl = "~/IRDistWiseReportDrillDown.aspx?Distid=" + lblDist.Text + "&FromDate=" + txtFormDate.Text + "&ToDate=" + txtToDate.Text + "&EntType=" + ddlEnterPriseType.SelectedItem.Text;
+
+                //if (lnkPending.Text != "0")
+                //    lnkPending.PostBackUrl = "~/IRDistWiseReportDrillDown.aspx?Distid=" + lblDist.Text + "&FromDate=" + txtFormDate.Text + "&ToDate=" + txtToDate.Text + "&EntType=" + ddlEnterPriseType.SelectedItem.Text;
+
+                //if (lnkQuery.Text != "0")
+                //    lnkQuery.PostBackUrl = "~/IRDistWiseReportDrillDown.aspx?Distid=" + lblDist.Text + "&FromDate=" + txtFormDate.Text + "&ToDate=" + txtToDate.Text + "&EntType=" + ddlEnterPriseType.SelectedItem.Text;
+
+                //if (lnkcommpending.Text != "0")
+                //    lnkcommpending.PostBackUrl = "~/IRDistWiseReportDrillDown.aspx?Distid=" + lblDist.Text + "&FromDate=" + txtFormDate.Text + "&ToDate=" + txtToDate.Text + "&EntType=" + ddlEnterPriseType.SelectedItem.Text;
+
+                //if (lnkApproved.Text != "0")
+                //    lnkApproved.PostBackUrl = "~/IRDistWiseReportDrillDown.aspx?Distid=" + lblDist.Text + "&FromDate=" + txtFormDate.Text + "&ToDate=" + txtToDate.Text + "&EntType=" + ddlEnterPriseType.SelectedItem.Text;
+
+                //if (lnkRejected.Text != "0")
+                //    lnkRejected.PostBackUrl = "~/IRDistWiseReportDrillDown.aspx?Distid=" + lblDist.Text + "&FromDate=" + txtFormDate.Text + "&ToDate=" + txtToDate.Text + "&EntType=" + ddlEnterPriseType.SelectedItem.Text;
+
+                //if (lnkCommQuery.Text != "0")
+                //    lnkCommQuery.PostBackUrl = "~/IRDistWiseReportDrillDown.aspx?Distid=" + lblDist.Text + "&FromDate=" + txtFormDate.Text + "&ToDate=" + txtToDate.Text + "&EntType=" + ddlEnterPriseType.SelectedItem.Text;
+
+
+
+
+                //string url = "~/Dept/Reports/IRDistWiseReportDrillDown";
+                //Response.Redirect(url);
+
+
+            }
+            if (e.Row.RowType == DataControlRowType.Footer)
+            {
+                string district;
+
+                if (ddldistrict.SelectedItem.Text == "" || ddldistrict.SelectedItem.Text == null || ddldistrict.SelectedItem.Text == "--Select--" || ddldistrict.SelectedValue == "0")
+                {
+                    district = "%";
+                }
+                else
+                {
+                    district = ddldistrict.SelectedItem.Text;
+                }
+
+                e.Row.Font.Bold = true;                
+                e.Row.Cells[2].Text = "Total";
+              //  LinkButton Total = new LinkButton();
+              //  Total.ForeColor = System.Drawing.Color.Blue;
+              //  Total.PostBackUrl = "~/IRDistWiseReportDrillDown.aspx?Distid=" + district + "&FromDate=" + txtFormDate.Text + "&ToDate=" + txtToDate.Text + "&EntType=" + ddlEnterPriseType.SelectedItem.Text;
+              //  Total.Text= TotalAppl.ToString();
+                e.Row.Cells[3].Text = TotalAppl.ToString();
+              //  e.Row.Cells[3].Controls.Add(Total);
+                e.Row.Cells[4].Text = ImaPending.ToString();
+                e.Row.Cells[5].Text = ImaQuery.ToString();
+                e.Row.Cells[6].Text = CommPending.ToString();
+                e.Row.Cells[7].Text = CommApproved.ToString();
+                e.Row.Cells[8].Text = CommRejected.ToString();
+                e.Row.Cells[9].Text = CommQuery.ToString();
+            }
+        }
+    }
+}
---
+++ MeghalayaUIP/Dept/Reports/IRDistWiseReportDrillDown.aspx
@@ -0,0 +1,85 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Dept/dept.Master" AutoEventWireup="true" CodeBehind="IRDistWiseReportDrillDown.aspx.cs" Inherits="MeghalayaUIP.Dept.Reports.IRDistWiseReportDrillDown" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+      <link href="../assets/admin/css/user.css" rel="stylesheet" />
+
+    <%--<nav aria-label="breadcrumb">
+        <ol class="breadcrumb d-flex justify-content-between">
+            <li class="breadcrumb-item active" aria-current="page">&nbsp;</li>
+            <li><a href="#"><span class="badge bg-primary text-white fw-bold">Visit Guidance Site</span></a></li>
+        </ol>
+    </nav>--%>
+    <div class="page-wrapper griddesignmulticount">
+        <div class="content container-fluid">           
+            <div class="card" id="Applications" runat="server" visible="false">
+                <div class="card-header d-flex justify-content-between">
+                    <h4 class="card-title mt-1"><b>Welcome to Dashboard </b></h4>
+                    <h4 class="card-title mt-1">
+                        <label id="unitname" runat="server"></label>
+                    </h4>
+                </div>
+
+                <div>
+                    <div class="col-md-12 ">
+                        <div id="success" runat="server" visible="false" class="alert alert-success alert-dismissible fade show" align="Center">
+                            <strong>Success!</strong><asp:Label ID="lblmsg" runat="server"></asp:Label>
+                            <asp:Label ID="Label2" runat="server"></asp:Label>
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">×</span></button>
+                        </div>
+                    </div>
+                    <div class="col-md-12 ">
+                        <div id="Failure" runat="server" visible="false" class="alert alert-danger alert-dismissible fade show" align="Center">
+                            <strong>Warning!</strong>
+                            <asp:Label ID="lblmsg0" runat="server"></asp:Label>
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">×</span>
+                            </button>
+                        </div>
+                    </div>
+                    <asp:HiddenField ID="hdnPreRegUNITID" runat="server" />
+                    <asp:HiddenField ID="hdnPreRegUID" runat="server" />
+                    <asp:HiddenField ID="hdnUserID" runat="server" />
+                </div>
+                <section id="dashboardcount" class="mt-2">
+                    <div class="container-fluid">
+                        <div class="table-responsive">
+                            <asp:GridView ID="GVDistWise" runat="server" AutoGenerateColumns="False" BorderColor="#003399" ShowHeaderWhenEmpty="true"
+                                BorderStyle="Solid" BorderWidth="1px" CellPadding="4" CssClass="table table-bordered table-striped table-hover" ForeColor="#333333"
+                                GridLines="None" Width="100%" EnableModelValidation="True">
+                                <RowStyle />
+                                <HeaderStyle BackColor="#013161" Font-Bold="True" ForeColor="White" Height="33px" HorizontalAlign="Center" />
+
+                                <Columns>
+                                    <asp:TemplateField HeaderText="Sl.No" ItemStyle-Width="5%">
+                                        <HeaderStyle HorizontalAlign="Center" />
+                                        <ItemStyle HorizontalAlign="Center" />
+                                        <ItemTemplate>
+                                            <%# Container.DataItemIndex + 1%>
+                                        </ItemTemplate>
+                                    </asp:TemplateField>
+                                    <%--<asp:CommandField HeaderText="Delete" ShowDeleteButton="True" ItemStyle-BackColor="Wheat" ItemStyle-ForeColor="WindowText" />--%>
+                                    <asp:BoundField HeaderText="Acknowledgement ID" DataField="PREREGUIDNO" HeaderStyle-HorizontalAlign="Center" ItemStyle-HorizontalAlign="Center" ItemStyle-ForeColor="WindowText" Visible="true" ItemStyle-Width="10%" />
+                                    <asp:BoundField HeaderText="Unit Name" DataField="COMPANYNAME" HeaderStyle-HorizontalAlign="Center" ItemStyle-HorizontalAlign="Center" ItemStyle-ForeColor="WindowText" ItemStyle-Width="10%" />
+                                    <asp:BoundField HeaderText="District" DataField="UNITADDRESS" HeaderStyle-HorizontalAlign="Left" ItemStyle-HorizontalAlign="Left" ItemStyle-ForeColor="WindowText" ItemStyle-Width="10%" />
+                                    <asp:BoundField HeaderText="Unit Address" DataField="UNITADDRESS" HeaderStyle-HorizontalAlign="Left" ItemStyle-HorizontalAlign="Left" ItemStyle-ForeColor="WindowText" ItemStyle-Width="30%" />
+                                    <asp:BoundField HeaderText="Enterprise" DataField="UNITADDRESS" HeaderStyle-HorizontalAlign="Left" ItemStyle-HorizontalAlign="Left" ItemStyle-ForeColor="WindowText" ItemStyle-Width="10%" />
+                                    <asp:BoundField HeaderText="Status" DataField="UNITADDRESS" HeaderStyle-HorizontalAlign="Left" ItemStyle-HorizontalAlign="Left" ItemStyle-ForeColor="WindowText" ItemStyle-Width="10%" />
+                                  
+<%--                                    <asp:TemplateField Visible="false">
+                                        <ItemTemplate>
+                                            <asp:Label ID="lblUnitID" Text='<%#Eval("UNITID")%>' runat="server"></asp:Label>
+                                        </ItemTemplate>
+                                    </asp:TemplateField>--%>
+
+                                </Columns>
+                            </asp:GridView>
+                        </div>
+                    </div>
+                </section>
+            </div>
+
+        </div>
+    </div>
+</asp:Content>
