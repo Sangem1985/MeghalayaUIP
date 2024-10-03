dde04c6587be00bb48f25cbf19f4a02c69e97f11
Sowjanya Chinni <sowjanya128@gmail.com>
2024-10-03 18:35:37 +0530

    usermaster

--- MeghalayaUIP/User/user.Master.cs
+++ MeghalayaUIP/User/user.Master.cs
@@ -44,6 +44,7 @@ namespace MeghalayaUIP.User
                     {
                         fnSetNewControls_Click(sender, e);
                     }*/
+                    masterball.InsPageAccessed(ObjUserInfo.Userid, ObjUserInfo.Email, Request.Url.ToString(), getclientIP());
                 }
 
             }
@@ -170,5 +171,24 @@ namespace MeghalayaUIP.User
         {
             Response.Redirect("~/User/Renewal/RENUserDashboard.aspx");
         }
+        public static string getclientIP()
+        {
+            string result = string.Empty;
+            string ip = HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+            if (!string.IsNullOrEmpty(ip))
+            {
+                string[] ipRange = ip.Split(',');
+                int le = ipRange.Length - 1;
+                result = ipRange[0];
+            }
+            else
+            {
+                result = HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
+            }
+
+            return result;
+        }
+
     }
+}
 }
