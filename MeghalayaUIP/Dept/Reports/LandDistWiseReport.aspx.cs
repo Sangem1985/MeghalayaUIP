c42aa2ec6ba6b4f0e2a0edbe162c2b48cf062de6
YADAGIRI <akhilakki058@gmail.com>
2024-10-11 12:27:29 +0530

    Grievance

---
+++ MeghalayaUIP/Dept/Reports/LandDistWiseReport.aspx.cs
@@ -0,0 +1,113 @@
+﻿using MeghalayaUIP.BAL.CommonBAL;
+using MeghalayaUIP.BAL.ReportBAL;
+using MeghalayaUIP.Common;
+using MeghalayaUIP.CommonClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MeghalayaUIP.Dept.Reports
+{
+    public partial class LandDistWiseReport : System.Web.UI.Page
+    {
+        DeptUserInfo ObjUserInfo = new DeptUserInfo();
+        MasterBAL mstrBAL = new MasterBAL();
+        ReportBAL reportsBAL = new ReportBAL();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+
+                if (Session["DeptUserInfo"] != null)
+                {
+                    if (Session["DeptUserInfo"] != null && Session["DeptUserInfo"].ToString() != "")
+                    {
+                        ObjUserInfo = (DeptUserInfo)Session["DeptUserInfo"];
+                    }
+                    if (hdnUserID.Value == "")
+                    {
+                        hdnUserID.Value = ObjUserInfo.UserID;
+                    }
+
+                    Page.MaintainScrollPositionOnPostBack = true;
+                    Failure.Visible = false;
+                    success.Visible = false;
+                    if (!IsPostBack)
+                    {
+                        BindDistricts();
+
+                        //btnsubmit_Click(sender, e);
+                    }
+                }
+                else
+                {
+                    Response.Redirect("~/DeptLogin.aspx");
+                }
+            }
+            catch (Exception ex)
+            {
+                lblmsg0.Text = "Oops, You've have encountered an error!! please contact administrator.";
+                Failure.Visible = true;
+                MGCommonClass.LogerrorDB(ex, HttpContext.Current.Request.Url.AbsoluteUri, hdnUserID.Value);
+            }
+        }
+
+        protected void ddldistrict_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        protected void BindDistricts()
+        {
+            try
+            {
+
+                ddldistrict.Items.Clear();
+                List<MasterDistrcits> objDistrictModel = new List<MasterDistrcits>();
+                string strmode = string.Empty;
+                strmode = "";
+                objDistrictModel = mstrBAL.GetDistrcits();
+                if (objDistrictModel != null)
+                {
+                    ddldistrict.DataSource = objDistrictModel;
+                    ddldistrict.DataValueField = "DistrictId";
+                    ddldistrict.DataTextField = "DistrictName";
+                    ddldistrict.DataBind();
+
+                }
+                else
+                {
+                    ddldistrict.DataSource = null;
+                    ddldistrict.DataBind();
+
+
+                }
+                AddSelect(ddldistrict);
+
+
+            }
+            catch (Exception ex)
+            {
+                lblmsg0.Text = ex.Message;
+                Failure.Visible = true;
+                MGCommonClass.LogerrorDB(ex, HttpContext.Current.Request.Url.AbsoluteUri, hdnUserID.Value);
+            }
+        }
+        public void AddSelect(DropDownList ddl)
+        {
+            try
+            {
+                System.Web.UI.WebControls.ListItem li = new System.Web.UI.WebControls.ListItem();
+                li.Text = "--ALL--";
+                li.Value = "%";
+                ddl.Items.Insert(0, li);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
