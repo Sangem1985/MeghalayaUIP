1d75aaeffcac08f1958f271ad22d1639c5044191
Sowjanya Chinni <sowjanya128@gmail.com>
2024-10-14 15:19:33 +0530

    notifications page

--- MeghalayaUIP/Notifications.aspx.cs
+++ MeghalayaUIP/Notifications.aspx.cs
@@ -4,13 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using MeghalayaUIP.BAL.CommonBAL;
+
 
 namespace MeghalayaUIP
 {
     public partial class Notifications : System.Web.UI.Page
     {
+        MasterBAL mbal = new MasterBAL();
+        protected string Notification_6May2022;
         protected void Page_Load(object sender, EventArgs e)
         {
+            Notification_6May2022 = mbal.EncryptFilePath("D:/Meghalaya/Documents/Notification_6May2022.pdf");
 
         }
     }
