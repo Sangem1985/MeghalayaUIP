1c72a8ca739f6d07c7f136d6dc54fa72632ce20f
madhuri sangem <sangem.madhuri@gmail.com>
2024-10-14 15:55:15 +0530

    outer master

--- MeghalayaUIP/Home.aspx.cs
+++ MeghalayaUIP/Home.aspx.cs
@@ -12,9 +12,13 @@ namespace MeghalayaUIP
     {
         MasterBAL objmgbal = new MasterBAL();
         protected string IncentivePackageDemo027082024 { get; set; }
+        protected string unnati2024 { get; set; }
+        protected string mipp2024 { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
             IncentivePackageDemo027082024 = objmgbal.EncryptFilePath("D:/Meghalaya/Documents/Incentive Package Demo 027082024.pdf");
+            unnati2024= objmgbal.EncryptFilePath("D:/Meghalaya/Documents/unnati2024.pdf");
+            mipp2024 = objmgbal.EncryptFilePath("D:/Meghalaya/Documents/mipp2024.pdf"); 
         }
     }
 }
