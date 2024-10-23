ba5ec2969be72e959b3cc4f841f29ba0ef9dfe1d
YADAGIRI <akhilakki058@gmail.com>
2024-10-23 14:42:39 +0530

    CFE

---
+++ MeghalayaUIP/Dept/PreReg/PreRegApplDPRDashBoard.aspx.cs
@@ -0,0 +1,99 @@
+﻿using MeghalayaUIP.BAL.PreRegBAL;
+using MeghalayaUIP.Common;
+using MeghalayaUIP.CommonClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MeghalayaUIP.Dept.PreReg
+{
+    public partial class PreRegApplDPRDashBoard : System.Web.UI.Page
+    {
+        PreRegBAL PreBAL = new PreRegBAL();
+        PreRegDtls prd = new PreRegDtls();
+        DeptUserInfo ObjUserInfo = new DeptUserInfo();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Session["DeptUserInfo"] != null)
+                {
+
+                    if (Session["DeptUserInfo"] != null && Session["DeptUserInfo"].ToString() != "")
+                    {
+                        ObjUserInfo = (DeptUserInfo)Session["DeptUserInfo"];
+                    }
+                    hdnUserID.Value = ObjUserInfo.UserID;
+                }
+                Page.MaintainScrollPositionOnPostBack = true;
+                Failure.Visible = false;
+                success.Visible = false;
+                if (!IsPostBack)
+                {
+                   // Bind();
+                }
+            }
+            catch (Exception ex)
+            {
+                Failure.Visible = true;
+                lblmsg0.Text = ex.Message;
+                MGCommonClass.LogerrorDB(ex, HttpContext.Current.Request.Url.AbsoluteUri, hdnUserID.Value);
+
+            }
+        }
+
+        protected void linkTotal_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(lblTotalApp.Text != "0")
+                {
+                    Response.Redirect("PreRegApplDPRView.aspx?status=DPRTOTAL");
+                }
+            }
+            catch(Exception ex)
+            {
+                Failure.Visible = true;
+                lblmsg0.Text = ex.Message;
+                MGCommonClass.LogerrorDB(ex, HttpContext.Current.Request.Url.AbsoluteUri, hdnUserID.Value);
+            }
+        }
+
+        protected void linktobeProc_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(lblDPRTOBEPROCESSED.Text != "0")
+                {
+                    Response.Redirect("PreRegApplDPRView.aspx?status=DPRTOBEPROCESSED");
+                }
+            }
+            catch(Exception ex)
+            {
+                Failure.Visible = true;
+                lblmsg0.Text = ex.Message;
+                MGCommonClass.LogerrorDB(ex, HttpContext.Current.Request.Url.AbsoluteUri, hdnUserID.Value);
+            }
+        }
+
+        protected void linkProcessed_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (lblDPRPROCESSED.Text != "0")
+                {
+                    Response.Redirect("PreRegApplDPRView.aspx?status=DPRPROCESSED");
+                }
+            }
+            catch (Exception ex)
+            {
+                Failure.Visible = true;
+                lblmsg0.Text = ex.Message;
+                MGCommonClass.LogerrorDB(ex, HttpContext.Current.Request.Url.AbsoluteUri, hdnUserID.Value);
+            }
+        }
+    }
+}
--- MeghalayaUIP/User/CFE/CFEPowerCEIGDetails.aspx.cs
+++ MeghalayaUIP/User/CFE/CFEPowerCEIGDetails.aspx.cs
@@ -90,9 +90,9 @@ namespace MeghalayaUIP.User.CFE
                     if (Request.QueryString.Count > 0)
                     {
                         if (Convert.ToString(Request.QueryString[0]) == "N")
-                            Response.Redirect("~/User/CFE/CFEFireDetails.aspx?Next=" + "N");
+                            Response.Redirect("~/User/CFE/CFEForestDetails.aspx?Next=" + "N");
                         else if (Convert.ToString(Request.QueryString[0]) == "P")
-                            Response.Redirect("~/User/CFE/CFEPowerDetails.aspx?Previous=" + "P");
+                            Response.Redirect("~/User/CFE/CFEProffessionalTax.aspx?Previous=" + "P");
                     }
                 }
             }
