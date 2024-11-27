0a17eb28a942c16b69827faf49dba32cd5b6e7d8
SivaDhanush_K <sivadhanushkosuri9966@gmail.com>
2024-11-27 10:59:29 +0530

    incEligibility page push

---
+++ MeghalayaUIP/User/INCENTIVE/INCEligibility.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MeghalayaUIP.User.INCENTIVE
+{
+    public partial class INCEligibility : System.Web.UI.Page
+    {
+        int index;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Link1_Click(object sender, EventArgs e)
+        {
+            MVprereg.ActiveViewIndex = 0;
+            Link1.CssClass = "Underlined1";
+        }
+
+        protected void Link2_Click(object sender, EventArgs e)
+        {
+            MVprereg.ActiveViewIndex = 1;
+            Link2.CssClass = "Underlined2";
+        }
+
+        protected void MVprereg_ActiveViewChanged(object sender, EventArgs e)
+        {
+            index = MVprereg.ActiveViewIndex;
+            if (index == 0)
+            { Link1.CssClass = "Underlined1"; }
+            if (index == 1)
+            { Link2.CssClass = "Underlined2"; }
+            if (index == 2)
+            { Link3.CssClass = "Underlined3"; }
+        }
+
+        protected void Link3_Click(object sender, EventArgs e)
+        {
+            MVprereg.ActiveViewIndex = 2;
+            Link3.CssClass = "Underlined3";
+        }
+    }
+}
