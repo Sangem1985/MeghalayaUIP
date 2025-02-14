bfed856fddeca0faf491b55e7e7c2b00c523505e
Sowjanya Chinni <sowjanya128@gmail.com>
2025-02-14 23:11:14 +0530

    user amstre

--- MeghalayaUIP/User/user.Master.designer.cs
+++ MeghalayaUIP/User/user.Master.designer.cs
@@ -186,6 +186,15 @@ namespace MeghalayaUIP.User
         protected global::System.Web.UI.WebControls.LinkButton lnkCentralRep;
 
         /// <summary>
+        /// lnkCompliance control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton lnkCompliance;
+
+        /// <summary>
         /// LinkButton1 control.
         /// </summary>
         /// <remarks>
