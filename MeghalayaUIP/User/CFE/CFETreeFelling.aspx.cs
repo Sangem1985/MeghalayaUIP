8394fdce52b834bd6e98f6f2b498e35922931e79
PAPAVATH YADAGIRI <akhilakki058@gmail.com>
2025-03-10 23:10:33 +0530

    CFEFEELINGTREE

---
+++ MeghalayaUIP/User/CFE/CFETreeFelling.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MeghalayaUIP.User.CFE
+{
+    public partial class CFETreeFelling : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblFileNo.Text = "PFTMUN/2025/00016";
+            lblDate.Text = "19/02/2025";
+            lblGranted.Text = "test name";
+            lblAddress.Text = "MAWLAI MAWROH";
+            lblArea.Text = "dfdfdfdf";
+            lbltree.Text = "efef";
+            lblNumber.Text = "44";
+            lblMemo.Text = "PFTMUN/2025/00016";
+            lblNames.Text = "DFO Territorial";
+            lblDesignation.Text = " Divisional Forest Officer (T)";
+            lblIssueDate.Text = "19/02/2025";
+        }
+    }
+}
