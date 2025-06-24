f2a32f7671937914ca86f78b71d20724d6a703a4
YADAGIRIPAP <165276381+YADAGIRIPAP@users.noreply.github.com>
2025-06-24 19:05:57 +0530

    Admin

---
+++ MeghalayaUIP/Admin/AdminDashboard.aspx.cs
@@ -0,0 +1,38 @@
+﻿using MeghalayaUIP.Common;
+using MeghalayaUIP.CommonClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MeghalayaUIP.Admin
+{
+    public partial class AdminDashboard : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //try
+            //{
+
+            //    if (Session["UserInfo"] != null)
+            //    {
+            //        var ObjUserInfo = new UserInfo();
+            //        if (Session["UserInfo"] != null && Session["UserInfo"].ToString() != "")
+            //        {
+            //            ObjUserInfo = (UserInfo)Session["UserInfo"];
+            //            hdnUserID.Value = ObjUserInfo.Userid;
+
+            //        }
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    //lblmsg0.Text = ex.Message;
+            //    //Failure.Visible = true;
+            //    MGCommonClass.LogerrorDB(ex, HttpContext.Current.Request.Url.AbsoluteUri, hdnUserID.Value);
+            //}
+        }
+    }
+}
